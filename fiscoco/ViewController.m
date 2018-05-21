ed0c83fb756e1733935fae86658b1a5f8ee5cfa3
administrator <administrator@mayahaodeMac-mini.local>
2018-05-21 16:21:22 +0800

    Initial Commit

---
+++ fiscoco/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  fiscoco
+//
+//  Created by administrator on 2018/5/21.
+//  Copyright © 2018年 administrator. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
